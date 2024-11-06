535aa0e185c1988f02f45931fbd66dbeb48a1b9f
SzymonPilarczyk <162544272+SzymonPilarczyk@users.noreply.github.com>
2024-11-06 23:30:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,60 @@
+import re
 import urllib.request
 import urllib.parse
-import re
 
+# Wzorce wyrażeń regularnych
+wzorzec_odn_artykul = r'<li[^>]*>.*<a[^>]*href=\"(/wiki/(?![^"]*:)[^"]+)\"[^>]*title=\"([^"]+)\"[^>]*>.*</li>'
+wzorzec_odn_kategoria = r'<a[^>]*href=\"(/wiki/Kategoria:[^"]+)\"[^>]*title=\"([^"]+)\"[^>]*>'
+wzorzec_odn_obrazek = r'<img[^>]*src=\"(//upload\.wikimedia\.org/[^"]+)\"[^>]*/>'
+wzorzec_odn_zewnetrzny = r'<a[^>]*class=\"external[^"]*\"[^>]*href=\"([^"]+)\"[^>]*>'
+wzorzec_odn_wewn = r'<a[^>]*href=\"(/wiki/(?![^"]*:)[^"]+)\"[^>]*title=\"([^"]+)\"[^>]*>'
 
-# Funkcja do pobierania zawartości strony z kategorii
-def pobierz_zawartosc_kategorii(kategoria):
-    url = "https://pl.wikipedia.org/wiki/Kategoria:" + kategoria.replace(' ', '_')
-    fp = urllib.request.urlopen(url)
-    tbytes = fp.read()
-    txt = tbytes.decode("utf-8")
-    fp.close()
-    return txt
+# Funkcja do znajdowania wzorców w HTML
+def znajdz_wzorce(wzorzec, tekst, maks=5):
+    return re.findall(wzorzec, tekst)[:maks]
 
+# Zamiana spacji na "_"
+def generuj_url_kat(kategoria_nazwa):
+    return f'https://pl.wikipedia.org/wiki/Kategoria:{kategoria_nazwa.replace(" ", "_")}'
 
-# Funkcja do zbierania linków wewnętrznych z artykułów
-def pobierz_linki_wewnetrzne_z_artykulu(tresc_artykulu, maks=5):
-    linki_wewn = []
-    for link in re.findall('<a href="/wiki/([^"]+)"', tresc_artykulu):
-        if ":" in link: continue  # Pomija linki do kategorii i innych specjalnych stron
-        linki_wewn.append(urllib.parse.unquote(link).replace(' ', '_'))
-        if len(linki_wewn) >= maks:  # Maksymalna liczba linków
-            break
-    return linki_wewn
+# Główna część programu
+kat = input().strip()  # Wprowadź nazwę kategorii
+url = generuj_url_kat(kat)
 
+# Pobieranie artykułów z kategorii
+with urllib.request.urlopen(url) as fp:
+    html = fp.read().decode("utf-8")
 
-# Funkcja do pobierania artykułu na podstawie linku
-def pobierz_artykul(link):
-    url_artykulu = "https://pl.wikipedia.org/wiki/" + link
-    fp = urllib.request.urlopen(url_artykulu)
-    tbytes = fp.read()
-    txt = tbytes.decode("utf-8")
-    fp.close()
-    return txt
+artykuly = znajdz_wzorce(wzorzec_odn_artykul, html, maks=3)  # Pobieramy 3 artykuły
 
+# Przetwarzanie każdego artykułu
+for link_art, tytul_art in artykuly:
+    # Pobieranie zawartości artykułu
+    with urllib.request.urlopen("https://pl.wikipedia.org" + link_art) as fp:
+        tresc = fp.read().decode("utf-8")
 
-# Główna funkcja - pobieranie artykułów z kategorii i wyciąganie linków
-def main():
-    kat = "Miasta na prawach powiatu"  # Przykładowa kategoria
-    txt = pobierz_zawartosc_kategorii(kat)
+    # Linki wewnętrzne
+    linki_wewn = znajdz_wzorce(wzorzec_odn_wewn, tresc, maks=5)
+    print("Linki wewnętrzne:")
+    print(' | '.join([link[1] for link in linki_wewn]))
 
-    # Wyciąganie linków do artykułów z kategorii
-    wzor = r'<li>.*?<a href="/wiki/([^"]+)"[^>]*>([^<]+)</a>.*?</li>'
-    wynik = re.findall(wzor, txt)
+    # Obrazki
+    obrazki = znajdz_wzorce(wzorzec_odn_obrazek, tresc, maks=3)
+    print("Obrazki:")
+    print(' | '.join([obrazek[0] for obrazek in obrazki]))
 
-    # Zbieramy tylko 2 pierwsze artykuły z kategorii
-    artykuly = wynik[:2]
+    # Linki zewnętrzne (przypisy)
+    with urllib.request.urlopen("https://pl.wikipedia.org" + link_art) as fp:
+        przypisy_html = fp.read().decode("utf-8")
+    zewnetrzne_linki = znajdz_wzorce(wzorzec_odn_zewnetrzny, przypisy_html, maks=3)
+    print("Linki zewnętrzne:")
+    print(' | '.join([link for link in zewnetrzne_linki]))
 
-    for link_art, tytul_art in artykuly:
-        print(f"Przetwarzam artykuł: {tytul_art}")
+    # Kategorie
+    with urllib.request.urlopen("https://pl.wikipedia.org" + link_art) as fp:
+        kategorie_html = fp.read().decode("utf-8")
+    kategorie = znajdz_wzorce(wzorzec_odn_kategoria, kategorie_html, maks=3)
+    print("Kategorie:")
+    print(' | '.join([kategoria[1].removeprefix('Kategoria:') for kategoria in kategorie]))
 
-        # Pobieramy zawartość artykułu
-        tresc_artykulu = pobierz_artykul(link_art)
-
-        # Wyciąganie linków wewnętrznych
-        linki_wewn = pobierz_linki_wewnetrzne_z_artykulu(tresc_artykulu)
-        print("Linki wewnętrzne:", linki_wewn)
-
-
-if __name__ == '__main__':
-    main()
+    print("-" * 50)  # Separator między artykułami
